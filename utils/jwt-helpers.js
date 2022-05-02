93e2c05ca6f32e6dca27c59dbdc277a46e081cea
GiangNguyenn <giang.nguyen@skedulo.com>
2022-05-02 22:01:57 +0700

    fix jwt-helpers

--- utils/jwt-helpers.js
+++ utils/jwt-helpers.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 //Generate an access token and a refresh token for this database user
-function jwtTokens({ user_id, user_name, user_email }) {
-  const user = { user_id, user_name, user_email };
+function jwtTokens({ username }) {
+  const user = { username };
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '86400s' });
   return { accessToken };
 }
