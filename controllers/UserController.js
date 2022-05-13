c55012b1997f4749104f600c08b7ec5b22308bbd
GiangNguyenn <giang.nguyen@skedulo.com>
2022-05-13 14:06:18 +0700

    feat: delete user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,44 +1,64 @@
-const database = require('../models/database')
-const {isExistingAdmin, getUsers, getUsersWithStatus, getUsersNotWithStatus, updateUserStatus, checkInUser, getCheckInRecords} = require("../models/database");
+const database = require("../models/database");
+const {
+  isExistingAdmin,
+  getUsers,
+  getUsersWithStatus,
+  getUsersNotWithStatus,
+  updateUserStatus,
+  checkInUser,
+  getCheckInRecords,
+  deleteUser,
+} = require("../models/database");
 
 module.exports = {
-    getUsers: async (req, res) => {
-        const {status} = req.query;
-        if (status){
-            if (status.includes('-')){
-                console.log('status.substring(1, status.length)', status.substring(1, status.length))
-              const result = await getUsersNotWithStatus(status.substring(1, status.length))
-              return res.json(result.rows)
-            }
-            const result = await getUsersWithStatus(status)
-             return res.json(result.rows)
-         }
-        const result = await getUsers()
-        return res.json(result.rows)
-        },
-    getUser: async (req, res) => {
-        const {user_id} = req.query
-        const user = await isExistingAdmin(user_id);
-        if (user && user.rows.length) {
-            console.log(user.rows[0])
-            return res.json(user.rows[0])
-        }
-    },
-    updateUserStatus: async (req, res) => {
-        const {status, sid} = req.body;
-        if (status){
-            const result = await updateUserStatus(sid, status)
-            return res.json(result)
-        }
-    },
-    checkIn: async (req, res) => {
-        const {sid, temperature} = req.body;
-        const checkin_time = new Date().toISOString();
-        const result = await checkInUser(sid, temperature, checkin_time);
-        return res.send('success')
-    },
-    getCheckInRecords: async (req,res) => {
-        const result = await getCheckInRecords()
-        return res.json(result.rows)
-}
-}
+  getUsers: async (req, res) => {
+    const { status } = req.query;
+    if (status) {
+      if (status.includes("-")) {
+        console.log(
+          "status.substring(1, status.length)",
+          status.substring(1, status.length)
+        );
+        const result = await getUsersNotWithStatus(
+          status.substring(1, status.length)
+        );
+        return res.json(result.rows);
+      }
+      const result = await getUsersWithStatus(status);
+      return res.json(result.rows);
+    }
+    const result = await getUsers();
+    return res.json(result.rows);
+  },
+  getUser: async (req, res) => {
+    const { user_id } = req.query;
+    const user = await isExistingAdmin(user_id);
+    if (user && user.rows.length) {
+      console.log(user.rows[0]);
+      return res.json(user.rows[0]);
+    }
+  },
+  updateUserStatus: async (req, res) => {
+    const { status, sid } = req.body;
+    if (status) {
+      const result = await updateUserStatus(sid, status);
+      return res.json(result);
+    }
+  },
+  checkIn: async (req, res) => {
+    const { sid, temperature } = req.body;
+    const checkin_time = new Date().toISOString();
+    const result = await checkInUser(sid, temperature, checkin_time);
+    return res.send("success");
+  },
+  getCheckInRecords: async (req, res) => {
+    const result = await getCheckInRecords();
+    return res.json(result.rows);
+  },
+  deleteUser: async (req, res) => {
+    const { sid } = req.body;
+    console.log("sid", sid);
+    const result = await deleteUser(sid);
+    return res.json(result);
+  },
+};
--- storage/storage-config.js
+++ storage/storage-config.js
@@ -1,9 +1,9 @@
-const cloudinary = require('cloudinary').v2
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name: 'hgvwytoge',
-    api_key: '624427376627544',
-    api_secret: 'RadsmEQTHiqmax_sJxJ5fx2Kc-g'
+  cloud_name: "hgvwytoge",
+  api_key: "624427376627544",
+  api_secret: "RadsmEQTHiqmax_sJxJ5fx2Kc-g",
 });
 
 module.exports = cloudinary;
